cdad8f1ab66066ed9bac308733f293284fff35ae
Patrikas <patrestak@gmail.com>
2026-04-20 20:58:54 +0300

    pridetas public

--- func.h
+++ func.h
@@ -11,6 +11,28 @@ private:
     int egz_;
     double galutinis_vid_;
     double galutinis_med_;
+
+public:
+    Studentas()
+        : egz_(0), galutinis_vid_(0.0), galutinis_med_(0.0) {}
+
+    Studentas(const std::string& vardas, const std::string& pavarde)
+        : vardas_(vardas), pavarde_(pavarde), egz_(0), galutinis_vid_(0.0), galutinis_med_(0.0) {}
+
+    inline const std::string& vardas() const { return vardas_; }
+    inline const std::string& pavarde() const { return pavarde_; }
+    inline const std::vector<int>& nd() const { return nd_; }
+    inline int egz() const { return egz_; }
+    inline double galutinisVid() const { return galutinis_vid_; }
+    inline double galutinisMed() const { return galutinis_med_; }
+
+    inline void setVardas(const std::string& vardas) { vardas_ = vardas; }
+    inline void setPavarde(const std::string& pavarde) { pavarde_ = pavarde; }
+    inline void setEgz(int egz) { egz_ = egz; }
+    inline void setGalutinisVid(double galutinis_vid) { galutinis_vid_ = galutinis_vid; }
+    inline void setGalutinisMed(double galutinis_med) { galutinis_med_ = galutinis_med; }
+    inline void clearNd() { nd_.clear(); }
+    inline void addNd(int pazymys) { nd_.push_back(pazymys); }
 };
 
 
